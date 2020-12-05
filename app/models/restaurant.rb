7c6c9dfeb16141e46f644aca50fe69ca95ed38d7
DanCema <dan.cemachovic@edhec.com>
2020-12-05 18:35:04 +0100

    restaurant show + user show with main KPIs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,19 +14,19 @@ class Restaurant < ApplicationRecord
   validates :postal_code, presence: true
 
 
-  def batchs
+  def batchs #array of batchs for a restaurant
     Batch.joins(:starcodes, :stars).where({ stars: { restaurant: self } }).uniq
   end
 
-  def users
-    self.stars.pluck(:user_id).uniq.compact.map { |u| User.find(u) }
+  def users  #array of clients for a restaurant
+    User.joins(:stars).where(stars: { restaurant: self }).uniq
   end
 
-  def stars_count_available(user)
+  def stars_count_available(user) #count number of stars available for a restaurant and a specific client
     self.stars.where(status: 'available', user: user).count
   end
 
-  def stars_count_used(user)
+  def stars_count_used(user) #count number of stars used for a restaurant and a specific client
     self.stars.where(status: 'used', user: user).count
   end
 end
